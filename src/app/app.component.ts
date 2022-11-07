42e147ab0b420c073dca4066191c786cc1828159
Vaziha-Syed <syedvaziha3052001@gmail.com>
2022-11-07 09:39:05 +0530

    Assessment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { UsersComponent } from './users/users.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Assessment';
+  title: string = 'User-Names';
+  data:Array<any>
+  People:any;
+ 
+  constructor(){
+    this.data=[
+      { FirstName: 'Roshan', LastName: 'Syed', Age: '22'},
+      { FirstName: 'Vinay', LastName: 'Kumar', Age: '22'},
+      { FirstName: 'Syamala', LastName: 'Syam', Age: '21'},
+      { FirstName: 'Bose', LastName: 'Chowdary', Age: '22'},
+      { FirstName: 'Chandu', LastName: 'Arla', Age: '22'}
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
